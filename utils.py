57e83a110e02494d279de25825e893e2eed21bcb
bolaty <69979488+bolaty@users.noreply.github.com>
2024-03-26 18:44:42 +0000

    save 26/03/2024

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE
 def connect_database():
     try:
         # Connexion à la base de données SQL Server
-        """
+        #"""
         connection_string = (
             "DRIVER={ODBC Driver 17 for SQL Server};"
             f'SERVER={MYSQL_HOST};'
@@ -13,8 +13,8 @@ def connect_database():
             f'PWD={MYSQL_PASSWORD};'
             "TrustServerCertificate=no;"
         )
-        """
-        connection_string = f'DRIVER={{SQL Server}};SERVER={MYSQL_HOST};DATABASE={MYSQL_DATABASE};UID={MYSQL_USER};PWD={MYSQL_PASSWORD}'
+        #"""
+        #connection_string = f'DRIVER={{SQL Server}};SERVER={MYSQL_HOST};DATABASE={MYSQL_DATABASE};UID={MYSQL_USER};PWD={MYSQL_PASSWORD}'
         #connection_string = f'DRIVER=ODBC Driver 18 for SQL Server;SERVER={MYSQL_HOST};DATABASE={MYSQL_DATABASE};UID={MYSQL_USER};PWD={MYSQL_PASSWORD}'
         #connection_string = f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={MYSQL_HOST};DATABSE={MYSQL_DATABASE};UID={MYSQL_USER};PWD={MYSQL_PASSWORD};TrustServerCertificate=yes;Encrypt=no;'
         connection = pyodbc.connect(connection_string)
