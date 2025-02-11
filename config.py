217679d847e6c5d3ce608a6324276ea441f34394
JEAN-JOEL-BLISSE <82817296+JEAN-JOEL-BLISSE@users.noreply.github.com>
2025-02-11 22:17:18 +0000

    save du 11.02.25

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ MYSQL_DATABASE = 'dbVitalFinancepython'
 MYSQL_REPONSE = ''
 LIENAPISMS='http://162.19.152.91:702/'#'http://192.168.1.100:711/'
 """
-MYSQL_HOST = 'DESKTOP-F9PEV2V'
+MYSQL_HOST = 'DESKTOP-OAFUPID'
 MYSQL_USER = 'sa'
 MYSQL_PASSWORD = '123'
-MYSQL_DATABASE = 'dbVitalFinanceRemuci'
+MYSQL_DATABASE = 'dbBaseDeTestLocalCeci'
 MYSQL_REPONSE = ''
 LIENAPISMS='http://162.19.152.91:702/'#'http://192.168.1.100:711/'
 # Dictionnaire pour garder une trace des connexions des caissières
